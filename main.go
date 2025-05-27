d11223d02d0ce3ffad7b861f8b477b34a2ffe22f
0xYotta <ctf0000000@gmail.com>
2025-05-28 02:44:44 +0500

    server init

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("ready to go!")
+
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	server.ListenAndServe()
 }
